60d330c1874d8f77a892cffbd6613f9306dcd373
Michael Shapiro <mshapiro@cma.com>
2023-05-26 21:27:55 -0400

    Add project files.

---
+++ DatePickerBug/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace DatePickerBug;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        bigBug.MinimumDate = DateTime.MinValue;
+        bigBug.MaximumDate = DateTime.MaxValue;
+        
+        bigBug.MinimumDate = new DateTime(2023, 5, 22);
+        bigBug.MaximumDate = new DateTime(2023, 6, 4);
+        //bigBug.Date = new DateTime(2023, 5, 30);
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        bigBug.MinimumDate = DateTime.MinValue;
+        bigBug.MaximumDate = DateTime.MaxValue;
+
+        bigBug.MinimumDate = new DateTime(2023, 2, 13);
+        bigBug.MaximumDate = new DateTime(2023, 2, 26);
+        //bigBug.Date = new DateTime(2023, 2, 18);
+    }
+}
+
---
+++ DatePickerBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DatePickerBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
