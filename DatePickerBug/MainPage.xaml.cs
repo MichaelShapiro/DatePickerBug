ac29c4281fb02e5dfdcb2fd2e2c1a342aa6973a0
Michael Shapiro <mshapiro@cma.com>
2023-05-27 14:56:56 -0400

    DatePickerBug

--- DatePickerBug/MainPage.xaml.cs
+++ DatePickerBug/MainPage.xaml.cs
@@ -2,32 +2,18 @@
 
 public partial class MainPage : ContentPage
 {
+    int k = 0;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        bigBug.MinimumDate = DateTime.MinValue;
-        bigBug.MaximumDate = DateTime.MaxValue;
-
-        bigBug.MinimumDate = new DateTime(2023, 4, 20);
-        bigBug.MaximumDate = new DateTime(2023, 4, 29);
-        bigBug.Date = new DateTime(2023, 5, 30);
-    }
-
     private void Button_Clicked_1(object sender, EventArgs e)
     {
-        bigBug.MinimumDate = DateTime.MinValue;
-        bigBug.MaximumDate = DateTime.MaxValue;
-
         bigBug.MinimumDate = new DateTime(2023, 2, 13);
         bigBug.MaximumDate = new DateTime(2023, 2, 18);
+
         bigBug.Date = new DateTime(2023, 2, 15);
     }
-
-
 }
 
